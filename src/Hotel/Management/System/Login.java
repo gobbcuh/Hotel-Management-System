902ef3e3fb200728627c18b07a51dd249b5a8f4b
jasmin <jajalorino@gmail.com>
2025-06-12 00:03:18 +0800

    login class draft

--- src/Hotel/Management/System/Login.java
+++ src/Hotel/Management/System/Login.java
@@ -2,22 +2,78 @@ package Hotel.Management.System;
 
 import java.awt.Color;
 import java.awt.Font;
-
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class Login extends JFrame {
+public class Login extends JFrame implements ActionListener {
+
+    JTextField textField1;
+    JPasswordField passwordField1;
+    JButton b1, b2;
+
     Login() {
         JLabel label1 = new JLabel("Username");
         label1.setBounds(40, 20, 100, 30);
-        label1.setFont(new Font("Tahoma", Font.BOLD, 20)); // change font
+        label1.setFont(new Font("Tahoma", Font.BOLD, 16)); // change font
         label1.setForeground(Color.WHITE);
         add(label1);
+
+        JLabel label2 = new JLabel("Password");
+        label2.setBounds(40, 70, 100, 30);
+        label2.setFont(new Font("Tahoma", Font.BOLD, 16)); // change font
+        label2.setForeground(Color.WHITE);
+        add(label2);
+
+        textField1 = new JTextField();
+        textField1.setBounds(150, 20, 150, 30);
+        textField1.setForeground(Color.WHITE);
+        textField1.setBackground(new Color(26, 104, 110)); // change background color
+        add(textField1);
+
+        passwordField1 = new JPasswordField();
+        passwordField1.setBounds(150, 70, 150, 30);
+        passwordField1.setForeground(Color.WHITE);
+        passwordField1.setBackground(new Color(26, 104, 110)); // change background color
+        add(passwordField1);
+
+        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icon/login.png")); // upload image
+        Image i1 = imageIcon.getImage().getScaledInstance(150, 150, Image.SCALE_DEFAULT); // adjust if needed
+        JLabel label = new JLabel(imageIcon);
+        label.setBounds(150, 120, 150, 150); // adjust if needed
+        add(label);
+
+        b1 = new JButton("Login");
+        b1.setBounds(40, 300, 120, 30);
+        b1.setFont(new Font("Serif", Font.BOLD, 15)); // change font
+        b1.setBackground(Color.BLACK);
+        b1.setForeground(Color.WHITE);
+        b1.addActionListener(this);
+        add(b1);
+
+        b2 = new JButton("Cancel");
+        b2.setBounds(180, 300, 120, 30);
+        b2.setFont(new Font("Serif", Font.BOLD, 15)); // change font
+        b2.setBackground(Color.BLACK);
+        b2.setForeground(Color.WHITE);
+        b2.addActionListener(this);
+        add(b2);
 
         getContentPane().setBackground(new Color(3, 45, 48)); // change background color
         setLayout(null);
         setLocation(400, 40);
         setSize(450, 600);
         setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == b1) {
+
+        } else {
+            System.exit(102);
+        }
     }
 
     public static void main(String[] args) {
