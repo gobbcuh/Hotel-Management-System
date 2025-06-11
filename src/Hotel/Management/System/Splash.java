75a20bc6df21b0c60fe59e1fbd16fb3f55a138c5
jasmin <jajalorino@gmail.com>
2025-06-11 22:21:01 +0800

    added HRMS sample / backup

---
+++ src/Hotel/Management/System/Splash.java
@@ -0,0 +1,13 @@
+package Hotel.Management.System;
+
+import javax.swing.JFrame;
+
+public class Splash extends JFrame {
+    Splash() {
+        setSize(858, 680);
+    }
+
+    public static void main(String[] args) {
+        new Splash();
+    }
+}
