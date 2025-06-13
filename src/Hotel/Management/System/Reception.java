3645ae995aae0c2414a9b363599deb749e4dd202
jasmin <jajalorino@gmail.com>
2025-06-13 18:03:29 +0800

    labels completed

--- src/Hotel/Management/System/Reception.java
+++ src/Hotel/Management/System/Reception.java
@@ -58,7 +58,7 @@ public class Reception extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-
+                    new Room();
                 } catch (Exception E) {
                     E.printStackTrace();
                 }
--- src/Hotel/Management/System/Room.java
+++ src/Hotel/Management/System/Room.java
@@ -4,6 +4,8 @@ import net.proteanit.sql.DbUtils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 
 public class Room extends JFrame {
@@ -45,6 +47,19 @@ public class Room extends JFrame {
         back.setForeground(Color.WHITE);
         back.setBounds(200, 500, 120, 30);
         panel.add(back);
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+
+            }
+        });
+
+        JLabel room = new JLabel("Room No.");
+        room.setBounds(12, 15, 80, 19);
+        room.setForeground(Color.WHITE);
+        room.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(room);
 
         JLabel availability = new JLabel("Availability");
         availability.setBounds(119, 15, 80, 19);
@@ -70,7 +85,7 @@ public class Room extends JFrame {
         Bed.setFont(new Font("Tahoma", Font.BOLD, 14));
         panel.add(Bed);
 
-
+        setUndecorated(true);
         setLayout(null);
         setLocation(500, 100);
         setSize(900, 600);
