6f8d7492c730501640aa7af53512cd33f8fd0cca
jasmin <jajalorino@gmail.com>
2025-06-13 18:06:55 +0800

    department class

---
+++ src/Hotel/Management/System/Department.java
@@ -0,0 +1,17 @@
+package Hotel.Management.System;
+
+import javax.swing.*;
+
+public class Department extends JFrame {
+    Department() {
+
+        setLayout(null);
+        setLocation(550, 150);
+        setSize(700, 500);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Department();
+    }
+}
