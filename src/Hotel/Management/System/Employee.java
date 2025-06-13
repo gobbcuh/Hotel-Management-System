a3c6d020fd394502e18d8bafac1e47f6bb73f5fd
jasmin <jajalorino@gmail.com>
2025-06-13 18:53:08 +0800

    added JTable

--- src/Hotel/Management/System/Employee.java
+++ src/Hotel/Management/System/Employee.java
@@ -11,6 +11,12 @@ public class Employee extends JFrame {
         panel.setLayout(null);
         add(panel);
 
+        JTable table = new JTable();
+        table.setBounds(10, 34, 980, 450);
+        table.setForeground(Color.WHITE);
+        table.setBackground(new Color(3, 45, 48));
+        panel.add(table);
+
         setLayout(null);
         setLocation(430, 100);
         setSize(1000, 600);
