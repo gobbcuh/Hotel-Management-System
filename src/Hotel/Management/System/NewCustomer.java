ae7e6a9e5022684ddbf7b5661db27d7d7d9bb6c3
jasmin <jajalorino@gmail.com>
2025-06-14 01:03:49 +0800

    added labels and buttons

--- src/Hotel/Management/System/NewCustomer.java
+++ src/Hotel/Management/System/NewCustomer.java
@@ -2,14 +2,156 @@ package Hotel.Management.System;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.ResultSet;
+import java.util.Date;
 
 public class NewCustomer extends JFrame {
+    JComboBox comboBox;
+    JTextField textFieldNumber, TextName, TextCountry, TextDeposit;
+    JRadioButton r1, r2;
+    Choice c1;
+    JLabel date;
+    JButton add, back;
+
     NewCustomer() {
         JPanel panel = new JPanel();
         panel.setBounds(5, 5, 840, 500);
         panel.setLayout(null);
         panel.setBackground(new Color(3, 45, 48));
         add(panel);
+
+        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icon/logo.png")); // to be uploaded
+        Image image = imageIcon.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+        ImageIcon imageIcon1 = new ImageIcon(image);
+        JLabel imglabel = new JLabel(imageIcon1);
+        imglabel.setBounds(550, 150, 200, 200);
+        panel.add(imglabel);
+
+        JLabel labelName = new JLabel("New Customer Form");
+        labelName.setBounds(118, 11, 260, 53);
+        labelName.setFont(new Font("Tahoma", Font.BOLD, 20));
+        labelName.setForeground(Color.WHITE);
+        panel.add(labelName);
+
+        JLabel labelID = new JLabel("ID :");
+        labelID.setBounds(35, 76, 200, 14);
+        labelID.setForeground(Color.WHITE);
+        labelID.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        panel.add(labelID);
+
+        comboBox = new JComboBox(new String[] {"Passport", "PhilSys ID", "Voter's ID", "Driving License"});
+        comboBox.setBounds(271, 73, 150, 20);
+        comboBox.setBackground(new Color(3, 45, 48));
+        comboBox.setForeground(Color.WHITE);
+        comboBox.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        panel.add(comboBox);
+
+        JLabel labelNumber = new JLabel("Number :");
+        labelNumber.setBounds(35, 111, 200, 14);
+        labelNumber.setForeground(Color.WHITE);
+        labelNumber.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        panel.add(labelNumber);
+        textFieldNumber = new JTextField();
+        textFieldNumber.setBounds(271, 111, 150, 20);
+        panel.add(textFieldNumber);
+
+        JLabel labelname = new JLabel("Name :");
+        labelname.setBounds(35, 151, 200, 14);
+        labelname.setForeground(Color.WHITE);
+        labelname.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        panel.add(labelname);
+        TextName = new JTextField();
+        TextName.setBounds(271, 151, 150, 20);
+        panel.add(TextName);
+
+        JLabel labelGender = new JLabel("Sex :");
+        labelGender.setBounds(35, 191, 200, 14);
+        labelGender.setForeground(Color.WHITE);
+        labelGender.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        panel.add(labelGender);
+
+        r1 = new JRadioButton("Male");
+        r1.setFont(new Font("Tahoma", Font.BOLD, 14));
+        r1.setForeground(Color.WHITE);
+        r1.setBackground(new Color(3, 45, 48));
+        r1.setBounds(271, 191, 80, 12);
+        panel.add(r1);
+
+        r2 = new JRadioButton("Female");
+        r2.setFont(new Font("Tahoma", Font.BOLD, 14));
+        r2.setForeground(Color.WHITE);
+        r2.setBackground(new Color(3, 45, 48));
+        r2.setBounds(350, 191, 80, 12);
+        panel.add(r2);
+
+        JLabel labelCountry = new JLabel("Country :");
+        labelCountry.setBounds(35, 231, 200, 14);
+        labelCountry.setForeground(Color.WHITE);
+        labelCountry.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        panel.add(labelCountry);
+        TextCountry = new JTextField();
+        TextCountry.setBounds(271, 231, 150, 20);
+        panel.add(TextCountry);
+
+        JLabel labelRoom = new JLabel("Room Number :");
+        labelRoom.setBounds(35, 274, 200, 14);
+        labelRoom.setForeground(Color.WHITE);
+        labelRoom.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        panel.add(labelRoom);
+
+        c1 = new Choice();
+        try {
+            con c = new con();
+            ResultSet resultSet = c.statement.executeQuery("select * from room");
+            while(resultSet.next()) {
+                c1.add(resultSet.getString("room_number"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        c1.setBounds(271, 274, 150, 20);
+        c1.setFont(new Font("Tahoma", Font.BOLD, 14));
+        c1.setForeground(Color.WHITE);
+        c1.setBackground(new Color(3, 45, 48));
+        panel.add(c1);
+
+        JLabel labelCIS = new JLabel("Checked-In :");
+        labelCIS.setBounds(35, 316, 200, 14);
+        labelCIS.setForeground(Color.WHITE);
+        labelCIS.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        panel.add(labelCIS);
+
+        Date date1 = new Date();
+
+        date = new JLabel(date1.toString());
+        date.setBounds(271, 316, 200, 14);
+        date.setForeground(Color.WHITE);
+        date.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        panel.add(date);
+
+        JLabel labelDeposit = new JLabel("Deposit :");
+        labelDeposit.setBounds(35, 359, 200, 14);
+        labelDeposit.setForeground(Color.WHITE);
+        labelDeposit.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        panel.add(labelDeposit);
+        TextDeposit = new JTextField();
+        TextDeposit.setBounds(271, 359, 150, 20);
+        panel.add(TextDeposit);
+
+        add = new JButton("Add");
+        add.setBounds(100, 430, 120, 30);
+        add.setForeground(Color.WHITE);
+        add.setBackground(Color.BLACK);
+        panel.add(add);
+
+        back = new JButton("Back");
+        back.setBounds(260, 430, 120, 30);
+        back.setForeground(Color.WHITE);
+        back.setBackground(Color.BLACK);
+        panel.add(back);
+
 
         setLayout(null);
         setLocation(500, 150);
