ceca8b0cfd801eeebccc92e4dc244c806f98f819
jasmin <jajalorino@gmail.com>
2025-06-14 01:42:36 +0800

    jpanel added

--- src/Hotel/Management/System/SearchRoom.java
+++ src/Hotel/Management/System/SearchRoom.java
@@ -1,8 +1,20 @@
 package Hotel.Management.System;
 
-public class SearchRoom {
-    SearchRoom(){
+import javax.swing.*;
+import java.awt.*;
 
+public class SearchRoom extends JFrame {
+    SearchRoom(){
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(3, 45, 48));
+        panel.setBounds(5, 5, 690, 490);
+        panel.setLayout(null);
+        add(panel);
+
+        setLayout(null);
+        setLocation(500, 200);
+        setSize(700, 500);
+        setVisible(true);
     }
 
     public static void main(String[] args) {
