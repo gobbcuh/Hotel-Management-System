3c012e2f1c9d0d56ef0b1edc248deb5ec6d8c785
jasmin <jajalorino@gmail.com>
2025-06-13 19:51:21 +0800

    completed JLabels

--- src/Hotel/Management/System/Employee.java
+++ src/Hotel/Management/System/Employee.java
@@ -4,6 +4,8 @@ import net.proteanit.sql.DbUtils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 
 public class Employee extends JFrame {
@@ -29,6 +31,66 @@ public class Employee extends JFrame {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        JButton back = new JButton("Back");
+        back.setBounds(350, 500, 120, 30);
+        back.setBackground(Color.BLACK);
+        back.setForeground(Color.WHITE);
+        panel.add(back);
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+            }
+        });
+
+        JLabel name = new JLabel("Name");
+        name.setBounds(41, 11, 70, 19);
+        name.setForeground(Color.WHITE);
+        name.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(name);
+
+        JLabel Age = new JLabel("Age");
+        Age.setBounds(159, 11, 70, 19);
+        Age.setForeground(Color.WHITE);
+        Age.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(Age);
+
+        JLabel gender = new JLabel("Sex");
+        gender.setBounds(273, 11, 70, 19);
+        gender.setForeground(Color.WHITE);
+        gender.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(gender);
+
+        JLabel job = new JLabel("Job");
+        job.setBounds(416, 11, 70, 19);
+        job.setForeground(Color.WHITE);
+        job.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(job);
+
+        JLabel salary = new JLabel("Salary");
+        salary.setBounds(536, 11, 70, 19);
+        salary.setForeground(Color.WHITE);
+        salary.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(salary);
+
+        JLabel phone = new JLabel("Phone");
+        phone.setBounds(656, 11, 70, 19);
+        phone.setForeground(Color.WHITE);
+        phone.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(phone);
+
+        JLabel email = new JLabel("Email");
+        email.setBounds(786, 11, 70, 19);
+        email.setForeground(Color.WHITE);
+        email.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(email);
+
+        JLabel natID = new JLabel("PhilSys No.");
+        natID.setBounds(896, 11, 70, 19);
+        natID.setForeground(Color.WHITE);
+        natID.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(natID);
 
         setLayout(null);
         setLocation(430, 100);
