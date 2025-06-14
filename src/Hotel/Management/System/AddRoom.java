8d3d6560bf94ce87d9c0cf5f579805f2d83940e8
jasmin <jajalorino@gmail.com>
2025-06-15 00:48:18 +0800

    add room done

--- src/Hotel/Management/System/AddRoom.java
+++ src/Hotel/Management/System/AddRoom.java
@@ -14,74 +14,74 @@ public class AddRoom extends JFrame implements ActionListener {
     AddRoom() {
         JPanel panel = new JPanel();
         panel.setBounds(5, 5, 875, 490);
-        panel.setBackground(new Color(3, 45, 48));
+        panel.setBackground(new Color(250, 213, 213));
         panel.setLayout(null);
         add(panel);
 
         JLabel l1 = new JLabel("Add Rooms");
-        l1.setBounds(194, 10, 160, 22);
+        l1.setBounds(194, 23, 160, 22);
         l1.setFont(new Font("Tahoma", Font.BOLD, 22));
-        l1.setForeground(Color.WHITE);
+        l1.setForeground(Color.BLACK);
         panel.add(l1);
 
         JLabel l2 = new JLabel("Room Number");
         l2.setBounds(64, 70, 152, 22);
         l2.setFont(new Font("Tahoma", Font.BOLD, 14));
-        l2.setForeground(Color.WHITE);
+        l2.setForeground(Color.BLACK);
         panel.add(l2);
         t2 = new JTextField();
         t2.setBounds(200, 70, 156, 20);
         t2.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        t2.setForeground(Color.WHITE);
-        t2.setBackground(new Color(16, 108, 115));
+        t2.setForeground(Color.BLACK);
+        t2.setBackground(Color.WHITE);
         panel.add(t2);
 
         JLabel l3 = new JLabel("Availability");
         l3.setBounds(64, 110, 152, 22);
         l3.setFont(new Font("Tahoma", Font.BOLD, 14));
-        l3.setForeground(Color.WHITE);
+        l3.setForeground(Color.BLACK);
         panel.add(l3);
         t3 = new JComboBox(new String[] {"Available", "Occupied"});
         t3.setBounds(200, 110, 156, 20);
         t3.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        t3.setForeground(Color.WHITE);
-        t3.setBackground(new Color(16, 108, 115));
+        t3.setForeground(Color.BLACK);
+        t3.setBackground(Color.WHITE);
         panel.add(t3);
 
         JLabel l4 = new JLabel("Price");
         l4.setBounds(64, 150, 152, 22);
         l4.setFont(new Font("Tahoma", Font.BOLD, 14));
-        l4.setForeground(Color.WHITE);
+        l4.setForeground(Color.BLACK);
         panel.add(l4);
         t4 = new JTextField();
         t4.setBounds(200, 150, 156, 20);
         t4.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        t4.setForeground(Color.WHITE);
-        t4.setBackground(new Color(16, 108, 115));
+        t4.setForeground(Color.BLACK);
+        t4.setBackground(Color.WHITE);
         panel.add(t4);
 
         JLabel l5 = new JLabel("Cleaning Status");
         l5.setBounds(64, 190, 152, 22);
         l5.setFont(new Font("Tahoma", Font.BOLD, 14));
-        l5.setForeground(Color.WHITE);
+        l5.setForeground(Color.BLACK);
         panel.add(l5);
         t5 = new JComboBox(new String[] {"Cleaned", "Undone"});
         t5.setBounds(200, 190, 156, 20);
         t5.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        t5.setForeground(Color.WHITE);
-        t5.setBackground(new Color(16, 108, 115));
+        t5.setForeground(Color.BLACK);
+        t5.setBackground(Color.WHITE);
         panel.add(t5);
 
         JLabel l6 = new JLabel("Bed Type");
         l6.setBounds(64, 230, 152, 22);
         l6.setFont(new Font("Tahoma", Font.BOLD, 14));
-        l6.setForeground(Color.WHITE);
+        l6.setForeground(Color.BLACK);
         panel.add(l6);
         t6 = new JComboBox(new String[] {"Single Bed", "Double Bed"});
         t6.setBounds(200, 230, 156, 20);
         t6.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        t6.setForeground(Color.WHITE);
-        t6.setBackground(new Color(16, 108, 115));
+        t6.setForeground(Color.BLACK);
+        t6.setBackground(Color.WHITE);
         panel.add(t6);
 
         b1 = new JButton("Add");
@@ -98,15 +98,15 @@ public class AddRoom extends JFrame implements ActionListener {
         b2.addActionListener(this);
         panel.add(b2);
 
-        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icon/logo.png")); // to be uploaded
-        Image image = imageIcon.getImage().getScaledInstance(300, 300, Image.SCALE_DEFAULT);
+        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icon/covalent.png")); // to be uploaded
+        Image image = imageIcon.getImage().getScaledInstance(370, 200, Image.SCALE_DEFAULT);
         ImageIcon imageIcon1 = new ImageIcon(image);
         JLabel label = new JLabel(imageIcon1);
-        label.setBounds(500, 60, 300, 300);
+        label.setBounds(440, 130, 370, 200);
         panel.add(label);
 
         setUndecorated(true);
-        setLocation(100, 50);
+        setLocation(335, 120);
         setLayout(null);
         setSize(885, 500);
         setVisible(true);
--- src/Hotel/Management/System/Room.java
+++ src/Hotel/Management/System/Room.java
@@ -15,21 +15,21 @@ public class Room extends JFrame {
     Room() {
         JPanel panel = new JPanel();
         panel.setBounds(5, 5, 890, 590);
-        panel.setBackground(new Color(3, 45, 48));
+        panel.setBackground(new Color(250, 213, 213));
         panel.setLayout(null);
         add(panel);
 
-        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icon/logo.png")); // to be uploaded
-        Image image = imageIcon.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icon/covalent.png"));
+        Image image = imageIcon.getImage().getScaledInstance(295, 150, Image.SCALE_DEFAULT);
         ImageIcon imageIcon1 = new ImageIcon(image);
         JLabel label = new JLabel(imageIcon1);
-        label.setBounds(600, 200, 200, 200);
+        label.setBounds(550, 10, 295, 150);
         panel.add(label);
 
         table = new JTable();
-        table.setBounds(10, 40, 500, 400);
-        table.setBackground(new Color(3, 45, 48));
-        table.setForeground(Color.WHITE);
+        table.setBounds(20, 40, 500, 400);
+        table.setBackground(new Color(250, 213, 213));
+        table.setForeground(Color.BLACK);
         panel.add(table);
 
         try {
@@ -56,38 +56,38 @@ public class Room extends JFrame {
         });
 
         JLabel room = new JLabel("Room No.");
-        room.setBounds(12, 15, 80, 19);
-        room.setForeground(Color.WHITE);
+        room.setBounds(15, 15, 80, 19);
+        room.setForeground(Color.BLACK);
         room.setFont(new Font("Tahoma", Font.BOLD, 14));
         panel.add(room);
 
         JLabel availability = new JLabel("Availability");
-        availability.setBounds(119, 15, 80, 19);
-        availability.setForeground(Color.WHITE);
+        availability.setBounds(122, 15, 80, 19);
+        availability.setForeground(Color.BLACK);
         availability.setFont(new Font("Tahoma", Font.BOLD, 14));
         panel.add(availability);
 
         JLabel Clean = new JLabel("Clean Status");
-        Clean.setBounds(216, 15, 150, 19);
-        Clean.setForeground(Color.WHITE);
+        Clean.setBounds(219, 15, 150, 19);
+        Clean.setForeground(Color.BLACK);
         Clean.setFont(new Font("Tahoma", Font.BOLD, 14));
         panel.add(Clean);
 
         JLabel Price = new JLabel("Price");
-        Price.setBounds(330, 15, 80, 19);
-        Price.setForeground(Color.WHITE);
+        Price.setBounds(333, 15, 80, 19);
+        Price.setForeground(Color.BLACK);
         Price.setFont(new Font("Tahoma", Font.BOLD, 14));
         panel.add(Price);
 
         JLabel Bed = new JLabel("Bed Type");
-        Bed.setBounds(417, 15, 80, 19);
-        Bed.setForeground(Color.WHITE);
+        Bed.setBounds(420, 15, 80, 19);
+        Bed.setForeground(Color.BLACK);
         Bed.setFont(new Font("Tahoma", Font.BOLD, 14));
         panel.add(Bed);
 
         setUndecorated(true);
         setLayout(null);
-        setLocation(500, 100);
+        setLocation(350, 80);
         setSize(900, 600);
         setVisible(true);
     }
