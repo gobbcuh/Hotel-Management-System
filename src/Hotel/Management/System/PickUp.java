0c2091698ea036028d8b06fa26fc23d3de2ac6c8
jasmin <jajalorino@gmail.com>
2025-06-14 10:08:47 +0800

    added PUS label

--- src/Hotel/Management/System/PickUp.java
+++ src/Hotel/Management/System/PickUp.java
@@ -11,6 +11,13 @@ public class PickUp extends JFrame {
         panel.setLayout(null);
         add(panel);
 
+        JLabel PUS = new JLabel("Pick Up Service");
+        PUS.setBounds(90, 11, 89, 14);
+        PUS.setBackground(Color.WHITE);
+        PUS.setFont(new Font("Tahoma", Font.BOLD, 20));
+        panel.add(PUS);
+
+
         setLayout(null);
         setSize(800, 600);
         setLocation(500, 100);
