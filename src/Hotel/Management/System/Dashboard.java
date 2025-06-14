ac938a9ef288f3c10eb7bb6b35235c70a25ed9f1
jasmin <jajalorino@gmail.com>
2025-06-14 20:42:39 +0800

    dashboard UI done

--- src/Hotel/Management/System/Dashboard.java
+++ src/Hotel/Management/System/Dashboard.java
@@ -11,22 +11,22 @@ public class Dashboard extends JFrame implements ActionListener {
         super("Covalent Hotel");
 
         rec = new JButton("Reception");
-        rec.setBounds(425, 510, 140, 30);
+        rec.setBounds(330, 510, 140, 30);
         rec.setFont(new Font("Tahoma", Font.BOLD, 15));
-        rec.setBackground(new Color(255, 98, 0));   // color to be changed
-        rec.setForeground(Color.WHITE);
+        rec.setBackground(new Color(250, 213, 213));   // color to be changed
+        rec.setForeground(Color.BLACK);
         rec.addActionListener(this);
         add(rec);
 
         add = new JButton("Admin");
-        add.setBounds(880, 510, 140, 30);
+        add.setBounds(823, 510, 140, 30);
         add.setFont(new Font("Tahoma", Font.BOLD, 15));
-        add.setBackground(new Color(255, 98, 0));   // color to be changed
-        add.setForeground(Color.WHITE);
+        add.setBackground(new Color(250, 213, 213));   // color to be changed
+        add.setForeground(Color.BLACK);
         add.addActionListener(this);
         add(add);
 
-        ImageIcon i11 = new ImageIcon(ClassLoader.getSystemResource("icon/logo.png")); // to be uploaded
+        /* ImageIcon i11 = new ImageIcon(ClassLoader.getSystemResource("icon/logo.png")); // to be uploaded
         Image i2 = i11.getImage().getScaledInstance(200, 195, Image.SCALE_DEFAULT);
         ImageIcon imageIcon11 = new ImageIcon(i2);
         JLabel label1 = new JLabel(imageIcon11);
@@ -38,13 +38,13 @@ public class Dashboard extends JFrame implements ActionListener {
         ImageIcon imageIcon111 = new ImageIcon(i22);
         JLabel label11 = new JLabel(imageIcon111);
         label11.setBounds(400, 300, 200, 195);
-        add(label11);
+        add(label11); */
 
-        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icon/main.gif")); // to be uploaded
-        Image i1 = imageIcon.getImage().getScaledInstance(1950, 1090, Image.SCALE_DEFAULT);
+        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icon/dashboard.gif")); // to be uploaded
+        Image i1 = imageIcon.getImage().getScaledInstance(1280, 720, Image.SCALE_DEFAULT);
         ImageIcon imageIcon1 = new ImageIcon(i1);
         JLabel label = new JLabel(imageIcon1);
-        label.setBounds(0, 0, 1950, 1090);
+        label.setBounds(0, -20, 1280, 720);
         add(label);
 
         setLayout(null);
