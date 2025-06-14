d132b5c2bb138c5a4f69c1a0119a5813199e4ef9
jasmin <jajalorino@gmail.com>
2025-06-14 11:07:07 +0800

    added labels and no. database connection

--- src/Hotel/Management/System/UpdateCheck.java
+++ src/Hotel/Management/System/UpdateCheck.java
@@ -1,8 +1,56 @@
 package Hotel.Management.System;
 
-public class UpdateCheck {
-    UpdateCheck() {
+import javax.swing.*;
+import java.awt.*;
+import java.sql.ResultSet;
 
+public class UpdateCheck extends JFrame {
+    UpdateCheck() {
+        JPanel panel = new JPanel();
+        panel.setBounds(5, 5, 940, 490);
+        panel.setBackground(new Color(3, 45, 48));
+        panel.setLayout(null);
+        add(panel);
+
+        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icon/logo.png")); // to be uploaded
+        Image image = imageIcon.getImage().getScaledInstance(300, 300, Image.SCALE_DEFAULT);
+        ImageIcon imageIcon1 = new ImageIcon(image);
+        JLabel label = new JLabel(imageIcon1);
+        label.setBounds(500, 60, 300, 300);
+        panel.add(label);
+
+        JLabel label1 = new JLabel("Check-In Details");
+        label1.setBounds(124, 11, 222, 25);
+        label1.setFont(new Font("Tahoma", Font.BOLD, 20));
+        label1.setForeground(Color.WHITE);
+        panel.add(label1);
+
+        JLabel label2 = new JLabel("ID :");
+        label2.setBounds(25, 88, 46, 14);
+        label2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        label2.setForeground(Color.WHITE);
+        panel.add(label2);
+
+        Choice c = new Choice();
+        c.setBounds(248, 85, 140, 20);
+        panel.add(c);
+
+        try {
+            con C = new con();
+            ResultSet resultSet = C.statement.executeQuery("select * from customer");
+            while (resultSet.next()) {
+                c.add(resultSet.getString("number"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        setLayout(null);
+        setSize(950, 500);
+        setLocation(400, 200);
+        setVisible(true);
     }
 
     public static void main(String[] args) {
