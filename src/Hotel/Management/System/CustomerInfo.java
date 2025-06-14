b53f7aa9d88c2467ca506eb26763595080ca5930
jasmin <jajalorino@gmail.com>
2025-06-14 09:51:10 +0800

    other labels added

--- src/Hotel/Management/System/CustomerInfo.java
+++ src/Hotel/Management/System/CustomerInfo.java
@@ -31,10 +31,52 @@ public class CustomerInfo extends JFrame {
         }
 
         JLabel id = new JLabel("ID");
-        id.setBounds(31, 11, 46, 14);
+        id.setBounds(31, 11, 100, 14);
         id.setForeground(Color.WHITE);
         id.setFont(new Font("Tahoma", Font.BOLD, 14));
         panel.add(id);
+
+        JLabel number = new JLabel("Number");
+        number.setBounds(150, 11, 100, 14);
+        number.setForeground(Color.WHITE);
+        number.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(number);
+
+        JLabel name = new JLabel("Name");
+        name.setBounds(270, 11, 100, 14);
+        name.setForeground(Color.WHITE);
+        name.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(name);
+
+        JLabel gender = new JLabel("Sex");
+        gender.setBounds(360, 11, 100, 14);
+        gender.setForeground(Color.WHITE);
+        gender.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(gender);
+
+        JLabel country = new JLabel("Country");
+        country.setBounds(480, 11, 100, 14);
+        country.setForeground(Color.WHITE);
+        country.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(country);
+
+        JLabel room = new JLabel("Room");
+        room.setBounds(600, 11, 100, 14);
+        room.setForeground(Color.WHITE);
+        room.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(room);
+
+        JLabel Time = new JLabel("Check-In Time");
+        Time.setBounds(680, 11, 110, 14);
+        Time.setForeground(Color.WHITE);
+        Time.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(Time);
+
+        JLabel deposit = new JLabel("Deposit");
+        deposit.setBounds(800, 11, 100, 14);
+        deposit.setForeground(Color.WHITE);
+        deposit.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(deposit);
 
 
         setLayout(null);
