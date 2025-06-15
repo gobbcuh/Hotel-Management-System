881f5d3532ba3d00cc1ae3f9529fb38437c9c560
jasmin <jajalorino@gmail.com>
2025-06-15 23:32:24 +0800

    drafted

--- src/Hotel/Management/System/Department.java
+++ src/Hotel/Management/System/Department.java
@@ -12,28 +12,12 @@ public class Department extends JFrame {
     Department() {
         JPanel panel = new JPanel();
         panel.setBackground(new Color(250, 213, 213));
-        panel.setBounds(5, 5, 690, 490);
+        panel.setBounds(5, 5, 992, 590);
         panel.setLayout(null);
         add(panel);
 
-        JTable table = new JTable();
-        table.setBounds(100, 40, 500, 350);
-        table.setBackground(new Color(250, 213, 213));
-        table.setForeground(Color.BLACK);
-        panel.add(table);
-
-        try {
-            con c = new con();
-            String departmentInfo = "select * from department";
-            ResultSet resultSet = c.statement.executeQuery(departmentInfo);
-            table.setModel(DbUtils.resultSetToTableModel(resultSet));
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         JButton back = new JButton("Back");
-        back.setBounds(270, 410, 120, 30);
+        back.setBounds(45, 530, 120, 30);
         back.setBackground(Color.BLACK);
         back.setForeground(Color.WHITE);
         panel.add(back);
@@ -44,22 +28,17 @@ public class Department extends JFrame {
             }
         });
 
-        JLabel label1 = new JLabel("Department");
-        label1.setBounds(175, 11, 105, 20);
-        label1.setForeground(Color.BLACK);
-        label1.setFont(new Font("Tahoma", Font.BOLD, 14));
-        panel.add(label1);
-
-        JLabel label2 = new JLabel("Budget");
-        label2.setBounds(461, 11, 105, 20);
-        label2.setForeground(Color.BLACK);
-        label2.setFont(new Font("Tahoma", Font.BOLD, 14));
-        panel.add(label2);
+        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icon/covalent.gif"));
+        Image image = imageIcon.getImage().getScaledInstance(982, 580, Image.SCALE_DEFAULT);
+        ImageIcon imageIcon1 = new ImageIcon(image);
+        JLabel label = new JLabel(imageIcon1);
+        label.setBounds(0, 0, 982, 580);
+        panel.add(label);
 
         setUndecorated(true);
         setLayout(null);
-        setLocation(430, 100);
-        setSize(700, 500);
+        setLocation(275, 50);
+        setSize(1002, 600);
         setVisible(true);
     }
 
